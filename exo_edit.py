a0f9d63d3ed1eee1559c5948bbb716e33064b305
= <eeeaeeea61@gmail.com>
2023-09-03 21:23:57 +0900

    new file:   exo_edit.py 	new file:   main.py

---
+++ exo_edit.py
@@ -0,0 +1,124 @@
+import os
+import csv
+import configparser
+import copy
+import re
+import flet as ft
+
+
+def merge_config(config,section_content_data_list,csv_path):
+
+    element_name = None
+    element_value = None
+    element_column = None
+    element_insert_type = None
+    element_insert_value = None
+
+    #configの分離
+    #print(config.sections())
+    config_head = copy.deepcopy(config)
+    config_body = copy.deepcopy(config)
+    for section_name in config.sections():
+        if section_name == "exedit":
+            continue
+        config_head.remove_section(section_name)
+    config_body.remove_section(config.sections()[0])
+    output_config = copy.deepcopy(config_head)
+
+    #lengthの取得
+    end_frame = 0
+    start_frame = float('inf')
+    for section_name in config_body.sections():
+        try:
+            start = int(config_body[section_name]["start"])
+            start_frame = min(start,start_frame)
+            end = int(config_body[section_name]["end"])
+            end_frame = max(end,end_frame)
+        except:
+            pass
+    length = end_frame -int(start_frame) + 1
+
+    #print(config_body.sections())
+    #csvの読み込み
+    with open(file=csv_path,mode="r", newline='') as f:
+                reader = csv.reader(f)
+                header = next(reader)
+                data = list(reader)
+
+    #書き換え
+    csvlen = len(data)
+    i = 0
+    for l in range(csvlen):
+        for _, section_content_data in enumerate(section_content_data_list):#オブジェクト　1: テキストとか
+            for n,section in enumerate(section_content_data):# 0.0 0.1 とか
+                replace_dict = {}
+                if n == 0 :
+                    section_name = str(i)
+                else:
+                    section_name = f"{i}.{n-1}"
+                
+                for ft_row in section:# 各ft_row
+                    for n,ft_data_cell in enumerate(ft_row.cells):#それぞれの要素_text x とか
+                        content = ft_data_cell.content
+                        #print(content.value)
+                        if n == 0:#項目名
+                            element_name = content.value
+                        elif n == 1:#初期値
+                            element_value = content.value
+                        elif n == 2:#csvの列番号
+                            try:
+                                element_column = int(content.value)
+                            except:
+                                element_column = None
+                        elif n == 3:#挿入の種類選択
+                            element_insert_type = content.value
+                        elif n == 4:#挿入する句
+                            element_insert_value = content.value
+                        
+                    #print(element_name,element_value,element_column)
+                    #print(type(element_name))
+                    #何も入力しなくてもstart,endだけは書き換える
+                    if element_name == "start" or element_name ==  "end":
+                        element_value = str(int(element_value)+length*l)
+
+                    #テキストは変換する
+                    if element_name == "text":
+                        element_value = str_to_byt(element_name)
+                    #列番号があれば書き換えを行う
+                    #print(element_column)
+                    if element_column != None:
+                        if element_name == "text":
+                            element_value = str_to_byt(data[l][element_column-1])
+                        else:
+                            element_value = str(data[l][element_column-1])
+                    if element_insert_type == "文字":
+                        element1 = byt_to_str(element_value)
+                        element_value = element_insert_value.replace("{text}",element1)
+                        element_value = str_to_byt(element_value)
+                    if element_insert_type == "数字":
+                        #桁数を判定
+                        if "." in element_value:
+                            decimal_places = len(element_value.split(".")[1])
+                        else:
+                            decimal_places = 0
+
+                        element_value = element_insert_value.replace("{number}",element_value)
+                        if re.match(r'^[\d\s()+\-*/.]*', element_value):
+                            result = eval(element_value)
+                        element_value=str(round(result,decimal_places-1))
+
+                    replace_dict[element_name] = element_value
+                    
+                
+                output_config[section_name]=replace_dict
+            i +=1
+
+    return output_config
+
+def byt_to_str(text):
+    return bytes.fromhex(text).decode('utf-16-le').replace("\x00","")
+def str_to_byt(text):
+    short_byt = text.encode("utf-16-le").hex()
+    while len(short_byt)<4096:
+        short_byt += "0"
+    return(short_byt)
---
+++ main.py
@@ -0,0 +1,232 @@
+import flet as ft
+import os
+import csv
+import configparser
+
+
+import exo_edit
+
+def main(page: ft.Page):
+    csv_path = None
+    config = None
+    
+
+    def pick_file_result(e: ft.FilePickerResultEvent):
+        file_path = e.files[0].path
+        file_extension = os.path.splitext(file_path)[1]
+
+        #section_navオブジェクトとcontentのリストを作る
+        if file_extension == ".exo":
+            #初期化
+            section_nav_data.clear()
+            section_content_data_list.clear()
+            section_content_data.clear()
+
+            nonlocal config
+            config = configparser.ConfigParser()
+            # 大文字を区別する
+            config.optionxform = str
+            config.read(file_path)
+
+            #print(config)
+
+            count=0 #section[0.0][0.1]...のカウンタ
+            objects = [] #オブジェクトごとにsectionをまとめたリスト
+            for n,section in enumerate(config.sections()):
+
+                if n == 0:
+                    #data_ini.append([sec_info])
+                    object_sections =[]
+                elif int(float(section)) == count:
+                    object_sections.append(dict(config[section]))
+                elif int(float(section)) != count:
+                    objects.append(object_sections)
+                    object_sections =[]
+                    object_sections.append(dict(config[section]))
+                    count += 1
+                if n == len(config.sections()) - 1:
+                    objects.append(object_sections)
+                    #print(objects)
+
+                
+            #section_nav_dataの作成
+            for n,object_dict_list in enumerate(objects):
+                object_dict = {}
+                for d in reversed(object_dict_list):
+                    object_dict.update(d)
+                
+                name = object_dict['_name']
+                layer = object_dict['layer']
+                while len(layer) < 4:
+                    layer = " " + layer
+                section_nav_data.append(
+                    ft.ListTile(title=ft.Text(f"{layer} : {name}"),
+                                on_click =lambda _,n=n: change_section(n), 
+                                dense=True),
+                    )
+            #print(section_nav_data)
+            section_nav.update()
+
+
+
+            #section_content_dataの作成
+            for l,object_dict_list in enumerate(objects):
+                a = []
+                for m,object_dict in enumerate(object_dict_list):
+                    b = []    
+                    for n,(key, value) in enumerate(object_dict.items()):
+                        if len(value) == 4096 and key == "text":
+                            value = exo_edit.byt_to_str(value)
+                            add_string_option = True
+                        else:
+                            add_string_option = False
+                        b.append(
+                            ft.DataRow(
+                                cells =[
+                                    ft.DataCell(ft.Text(key)),
+                                    ft.DataCell(ft.Text(value)),
+                                    ft.DataCell(ft.TextField(
+                                        keyboard_type= "NUMBER",
+                                        on_focus=lambda _,l=l,m=m,n=n:csv_input_focus(l,m,n,2)
+                                    )),
+                                    ft.DataCell(
+                                        content=(ft.Dropdown(
+                                            options=[
+                                                ft.dropdown.Option("数字"),    
+                                            ],
+                                            border="NONE",
+                                            on_focus=lambda _,l=l,m=m,n=n:csv_input_focus(l,m,n,3)
+                                        ))
+                                    ),
+                                    ft.DataCell(
+                                        ft.TextField(
+                                            keyboard_type= "NUMBER",
+                                            on_focus=lambda _,l=l,m=m,n=n:csv_input_focus(l,m,n,4)
+                                        ),
+                                    ),
+                                ]
+                            )
+                        )
+                        if add_string_option:
+                            b[-1].cells[3].content.options.append(ft.dropdown.Option("文字"))
+                    a.append(b)   
+                section_content_data_list.append(a)
+                #print(section_content_data_list)
+        
+
+        if file_extension == ".csv":
+            nonlocal csv_path
+            csv_path = file_path
+            with open(file=file_path,mode="r", newline='') as f:
+                reader = csv.reader(f)
+                data = list(reader)
+            data_columns = []
+            data_rows=[]
+            #print(data)
+            for column in data[0]:
+                data_columns.append(ft.DataColumn(ft.Text(column)))
+            for row in data[1:]:
+                data_row = ft.DataRow()
+                for value in row:
+                    data_row.cells.append(ft.DataCell(ft.Text(value)),)
+                data_rows.append(data_row)
+
+            csv_table.columns = data_columns
+            csv_table.rows = data_rows
+            csv_table.update()
+
+    def pick_file_write_result(e: ft.FilePickerResultEvent):
+        file_path = e.path
+        print(config)
+        merged_config = exo_edit.merge_config(config,section_content_data_list,csv_path)
+
+        with open(file_path, 'w') as configfile:
+            # 設定をファイルに書き込む
+            merged_config.write(configfile, space_around_delimiters=False)
+
+    #表示を変えるイベント群
+    nav_folus_now = 0
+    def change_section(n):
+        nonlocal nav_folus_now
+        nav_folus_now = n
+        temp = section_content_data_list[nav_folus_now]
+        section_content.rows = [item for sublist in temp for item in sublist]
+        page.update()
+
+    field_focus_now=[]
+    def csv_input_focus(l,m,n,column):
+        nonlocal field_focus_now
+        field_focus_now = [l,m,n,column]
+    
+
+
+    #それぞれのオブジェクトを生成
+    #csv
+    csv_table = ft.DataTable(expand= 1)
+    csv_table_box = ft.ListView(expand=1)
+    csv_table_box.controls.append(csv_table)
+    #open button
+    filepicker = ft.FilePicker(on_result=pick_file_result)
+    read_button = ft.ElevatedButton(
+                    "Pick files",
+                    icon=ft.icons.FILE_OPEN,
+                    on_click=lambda _: filepicker.pick_files(allow_multiple=False,allowed_extensions =["exo","csv"])
+                )
+    #write button
+    filepicker_write = ft.FilePicker(on_result=pick_file_write_result)
+    write_button = ft.ElevatedButton(
+                    "Save files",
+                    icon=ft.icons.SAVE,
+                    on_click=lambda _: filepicker_write.save_file(allowed_extensions=["exo"])
+                )
+    #sections
+    section_nav_data = []
+    section_nav = ft.ListView(
+                width=200,
+                controls=section_nav_data,        
+            )
+    section_content_data_list = []
+    section_content_data = []
+    section_content = ft.DataTable(
+        expand=1,
+        column_spacing=5,
+        columns=[
+            ft.DataColumn(ft.Text("項目名",width=120),tooltip="項目名=値"),
+            ft.DataColumn(ft.Text("初期値",width=90)),
+            ft.DataColumn(ft.Text(".csvの列番号",width=90),tooltip=".csvの何列目と置き換えるか"),
+            ft.DataColumn(ft.Text("挿入",width=80),tooltip="選択して任意の値を入力"),
+            ft.DataColumn(ft.Text("挿入",width=200),tooltip="選択して任意の値を入力"),
+        ],
+        rows = [item for sublist in section_content_data for item in sublist]
+    )
+    section_content_box = ft.ListView(expand=1)
+    section_content_box.controls.append(section_content)
+    
+    page.overlay.append(filepicker)
+    page.overlay.append(filepicker_write)
+
+    #キーボードショートカット
+
+    def on_keyboard(e:ft.KeyboardEvent):
+        if e.key == "Enter" and e.shift:
+            change_section(nav_folus_now+1)
+        elif e.key == "Enter":
+            print(field_focus_now)
+            if field_focus_now[2]+1 >= len(section_content_data_list[field_focus_now[0]][field_focus_now[1]]):
+                section_content_data_list[field_focus_now[0]][field_focus_now[1]+1][0].cells[field_focus_now[3]].content.focus()
+            else:
+                section_content_data_list[field_focus_now[0]][field_focus_now[1]][field_focus_now[2]+1].cells[field_focus_now[3]].content.focus()
+    page.on_keyboard_event = on_keyboard
+
+    page.add(
+        ft.Row([read_button,write_button]),
+        ft.Row([
+            section_nav,
+            ft.VerticalDivider(width=1),
+            section_content_box,
+            csv_table_box,
+            ],
+            expand=True,) 
+    )
+
+ft.app(target=main)
